ad5ba9f1e02182e7fbabb6ca5a6aa047d6d8a6c8
Łukasz Kaźmierski <prywatnaapka@gmail.com>
2021-09-11 21:00:09 +0200

    chore: add typescript and moongose packages

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
+
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('API TODO APP'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.get('/', (req, res) => res.send('API TODO APP'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
